f51d6908eeabdb5d6a5c8952511419944ba4b8e0
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 15:38:32 +0800

    Amend the two numbers, `num1` and `num2`, to be doubles/decimals.

--- main.c
+++ main.c
@@ -3,14 +3,14 @@
 
 int main() {
     /* Declare two numbers */
-    int num1;
-    int num2;
+    double num1;
+    double num2;
     /* Enter numbers */
     printf("Enter first number: ");
-    scanf("%d", &num1);
+    scanf("%lf", &num1);
     printf("Enter second number: ");
-    scanf("%d", &num2);
+    scanf("%lf", &num2);
     /* Print answer (add both numbers together) */
-    printf("Answer: %d", num1 + num2);
+    printf("Answer: %f", num1 + num2);
     return 0;
 }
