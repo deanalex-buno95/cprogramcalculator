550c5865c20ac31af138546aab4aea60d5ecbd67
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 15:37:11 +0800

    Add the two numbers, `num1` and `num2`, together.

--- main.c
+++ main.c
@@ -10,5 +10,7 @@ int main() {
     scanf("%d", &num1);
     printf("Enter second number: ");
     scanf("%d", &num2);
+    /* Print answer (add both numbers together) */
+    printf("Answer: %d", num1 + num2);
     return 0;
 }
