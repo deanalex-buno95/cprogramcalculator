9fc7dad5053b346439f0488c9a2edfdc10904ac6
Dean Alexander Santos Buño <94216668+deanalex-buno95@users.noreply.github.com>
2023-07-12 15:34:43 +0800

    Enter the two numbers, `num1` and `num2`, for the calculator.

--- main.c
+++ main.c
@@ -5,5 +5,10 @@ int main() {
     /* Declare two numbers */
     int num1;
     int num2;
+    /* Enter numbers */
+    printf("Enter first number: ");
+    scanf("%d", &num1);
+    printf("Enter second number: ");
+    scanf("%d", &num2);
     return 0;
 }
